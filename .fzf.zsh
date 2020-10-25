f041acfe302f653dca8eca0c4ab2ce8e4509810d
JayD <jaydeepjd.1125@gmail.com>
2020-10-25 13:00:41 +0530

    Added Files

---
+++ .fzf.zsh
@@ -0,0 +1,13 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/linux/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/home/linux/.fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/home/linux/.fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/home/linux/.fzf/shell/key-bindings.zsh"
